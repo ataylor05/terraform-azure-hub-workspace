1e87586126d9626b4ac987a06c7fe0299f4cd624
Allan Taylor <allan@startmail.com>
2021-11-03 13:35:19 -0400

    Update terraform_azure_example_test.go

--- tests/terraform_azure_example_test.go
+++ tests/terraform_azure_example_test.go
@@ -9,19 +9,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// An example of how to test the Terraform module in examples/terraform-azure-example using Terratest.
 func TestTerraformAzureExample(t *testing.T) {
 	t.Parallel()
 
 	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
 		TerraformDir: "../azure-hub",
+		Vars: map[string]interface{}{
+			"resource_group_name": "terratest",
+		},
 	}
 
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Run `terraform output` to get the value of an output variable
@@ -34,4 +33,3 @@ func TestTerraformAzureExample(t *testing.T) {
 
 	// Test that the Virtual Machine size matches the Terraform specification
 	assert.Equal(t, expectedVMSize, actualVMSize)
-}
