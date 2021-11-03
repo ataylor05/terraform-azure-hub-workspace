d9f463ea4bee1467bcb3f60c4b46062683023f10
Allan Taylor <allan@startmail.com>
2021-11-03 13:36:48 -0400

    Update variables.tf

--- azure-hub/variables.tf
+++ azure-hub/variables.tf
@@ -1,24 +1,4 @@
-variable "subscription_id" {
+variable "resource_group_name" {
     type = string
-    description = "The Azure subscription id."
-}
-
-variable "aad_tenant_id" {
-    type = string
-    description = "The Azure AD tenant id."
-}
-
-variable "client_id" {
-    type = string
-    description = "The Azure service principal client id."
-}
-
-variable "client_secret" {
-    type = string
-    description = "The Azure service principal client secret."
-}
-
-variable "ptp_vpn_psk" {
-    type = string
-    description = "The VPN PSK."
+    description = "RG name."
 }
